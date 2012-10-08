60aca14b330d54580a62c31260d8e1ceb2f79cd2
sguazt <marco.guazzone@gmail.com>
2012-10-08 11:53:36 +0200

    (new:minor) Added to repo

---
+++ inc/dcs/testbed/workload_drivers.hpp
@@ -0,0 +1,9 @@
+#ifndef DCS_TESTBED_WORKLOAD_DRIVERS_HPP
+#define DCS_TESTBED_WORKLOAD_DRIVERS_HPP
+
+
+#include <dcs/testbed/base_workload_driver.hpp>
+#include <dcs/testbed/rain_workload_driver.hpp>
+
+
+#endif // DCS_TESTBED_WORKLOAD_DRIVERS_HPP
