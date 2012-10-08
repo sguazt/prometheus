e9a73969ed510e2ee5c10a4aa245b2af4053a6a5
sguazt <marco.guazzone@gmail.com>
2012-10-08 11:54:51 +0200

    (change:minor) Use of dcsxx-commons library.
    (change:minor) Changes according to recent commits.

--- src/olio_sysid.cpp
+++ src/olio_sysid.cpp
@@ -15,58 +15,38 @@
  *                           University of Piemonte Orientale,
  *                           Alessandria (Italy)]
  *
- * This file is part of Foobar.
+ * This file is part of dcsxx-testbed.
  *
- * Foobar is free software: you can redistribute it and/or modify
+ * dcsxx-testbed is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published
  * by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Foobar is distributed in the hope that it will be useful,
+ * dcsxx-testbed is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ * along with dcsxx-testbed.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <boost/numeric/ublas/vector.hpp>
 #include <boost/smart_ptr.hpp>
 #include <cstddef>
 #include <cstring>
+#include <dcs/logging.hpp>
 #include <dcs/testbed/signal_generators.hpp>
 #include <dcs/testbed/system_identification.hpp>
 #include <dcs/testbed/virtual_machines.hpp>
+#include <dcs/testbed/workload_drivers.hpp>
 #include <iostream>
 #include <string>
 #include <stdexcept>
+#include <vector>
 
 
 namespace /*<unnamed>*/ {
-
-
-void log_debug(std::string const& msg)
-{
-#ifndef NDEBUG
-	std::clog << "[D] " << msg << std::endl;
-#endif // NDEBUG
-}
-
-void log_info(std::string const& msg)
-{
-	std::clog << "[I] " << msg << std::endl;
-}
-
-void log_warn(std::string const& msg)
-{
-	std::clog << "[W] " << msg << std::endl;
-}
-
-void log_error(std::string const& msg)
-{
-	std::clog << "[E] " << msg << std::endl;
-}
 
 void usage(char const* progname)
 {
@@ -213,60 +193,64 @@ int main(int argc, char *argv[])
 		std::ostringstream oss;
 
 		oss << "OlioDB URI: " << oliodb_uri;
-		log_info(oss.str());
+		dcs::log_info(DCS_LOGGING_AT, oss.str());
 		oss.str("");
 
 		oss << "OlioDB VM name: " << oliodb_name;
-		log_info(oss.str());
+		dcs::log_info(DCS_LOGGING_AT, oss.str());
 		oss.str("");
 
 		oss << "OlioWeb URI: " << olioweb_uri;
-		log_info(oss.str());
+		dcs::log_info(DCS_LOGGING_AT, oss.str());
 		oss.str("");
 
 		oss << "OlioWeb VM name: " << olioweb_name;
-		log_info(oss.str());
+		dcs::log_info(DCS_LOGGING_AT, oss.str());
 		oss.str("");
 
 		oss << "Workload driver path: " << wkl_driver_path;
-		log_info(oss.str());
+		dcs::log_info(DCS_LOGGING_AT, oss.str());
 		oss.str("");
 	}
 
 	namespace testbed = ::dcs::testbed;
 	namespace ublas = ::boost::numeric::ublas;
 
+	typedef boost::shared_ptr< testbed::base_virtual_machine<real_type> > vm_pointer;
+
 	try
 	{
-		testbed::rain_workload_driver driver(wkl_driver_path);
-
-		boost::shared_ptr< testbed::base_virtual_machine<real_type> > p_oliodb_vm(new testbed::libvirt_virtual_machine<real_type>(oliodb_uri, oliodb_name));
-		boost::shared_ptr< testbed::base_virtual_machine<real_type> > p_olioweb_vm(new testbed::libvirt_virtual_machine<real_type>(olioweb_uri, olioweb_name));
+		vm_pointer p_oliodb_vm(new testbed::libvirt_virtual_machine<real_type>(oliodb_uri, oliodb_name));
+		vm_pointer p_olioweb_vm(new testbed::libvirt_virtual_machine<real_type>(olioweb_uri, olioweb_name));
 
 		const std::size_t nu(2);
+
+		std::vector<vm_pointer> vms(nu);
+		vms[0] = p_olioweb_vm;
+		vms[1] = p_oliodb_vm;
 
 		ublas::scalar_vector<real_type> ampl(nu, 0.5);
 		ublas::scalar_vector<real_type> freq(nu, 8);
 		ublas::scalar_vector<real_type> phase(nu, 0);
 		ublas::scalar_vector<real_type> bias(nu, 0.5);
 
+		boost::shared_ptr< testbed::base_workload_driver > p_driver(new testbed::rain_workload_driver(testbed::rain_workload_driver::olio_workload, wkl_driver_path));
+
 		boost::shared_ptr< testbed::base_signal_generator<real_type> > p_sig_gen(new testbed::sinusoidal_mesh_signal_generator<real_type>(ampl, freq, phase, bias));
 
-		testbed::system_identification<real_type> sysid(p_oliodb_vm, p_olioweb_vm, p_sig_gen);
-
-		driver.start();
+		testbed::system_identification<real_type> sysid(vms.begin(), vms.end(), p_driver, p_sig_gen);
 
 		sysid.run();
 	}
 	catch (std::exception const& e)
 	{
 		ret = 1;
-		log_error(e.what());
+		dcs::log_error(DCS_LOGGING_AT, e.what());
 	}
 	catch (...)
 	{
 		ret = 1;
-		log_error("Unknown error");
+		dcs::log_error(DCS_LOGGING_AT, "Unknown error");
 	}
 
 	return ret;
