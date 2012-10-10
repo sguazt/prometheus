21985293d4166aba6e1c53c261e9ad3908c26ce3
sguazt <marco.guazzone@gmail.com>
2012-10-10 08:26:40 +0200

    (new:major) * Added implementation for methods "ready" and "alive" * Added monitoring of underlying RAIN process by means of POSIX threads

--- inc/dcs/testbed/rain_workload_driver.hpp
+++ inc/dcs/testbed/rain_workload_driver.hpp
@@ -40,13 +40,19 @@
 #include <dcs/system/posix_process.hpp>
 #include <dcs/system/process_status_category.hpp>
 #include <dcs/testbed/base_workload_driver.hpp>
+#include <istream>
+extern "C" {
+#include <pthread.h>
+}
 #include <string>
 #include <vector>
 
 
 namespace dcs { namespace testbed {
 
-namespace detail { namespace /*<unnamed>*/ {
+namespace detail {
+
+namespace /*<unnamed>*/ {
 
 inline
 ::std::string make_java_command(::std::string const& java_home)
@@ -95,8 +101,6 @@ inline
 	args.push_back(rain_home + "/rain.jar:" + rain_home + "/workloads/" + workload + ".jar");
 	args.push_back("radlab.rain.Benchmark");
 	args.push_back(rain_home + "/config/rain.config." + workload + ".json");
-	args.push_back(">/dev/null");
-	args.push_back("2>&1");
 
 	return args;
 }
@@ -118,7 +122,11 @@ inline
 	return make_rain_args(workload, ".");
 }
 
-}} // Namespace detail::<unnamed>
+} // Namespace <unnamed>
+
+void* monitor_rain_rampup(void* arg);
+
+} // Namespace detail
 
 
 class rain_workload_driver: public base_workload_driver
@@ -127,22 +135,41 @@ class rain_workload_driver: public base_workload_driver
 	private: typedef ::boost::shared_ptr<sys_process_type> sys_process_pointer;
 
 
+	friend void* detail::monitor_rain_rampup(void*);
+
+
 	public: enum workload_category
 	{
 		olio_workload
 	};
 
 
+	public: rain_workload_driver()
+	: p_thread_(0),
+	  p_mutex_(0)
+	{
+	}
+
+	public: ~rain_workload_driver()
+	{
+		if (p_thread_)
+		{
+			::pthread_join(*p_thread_, 0);
+		}
+	}
+
 	public: rain_workload_driver(workload_category wkl_cat)
 	: cmd_(detail::make_java_command()),
-	  args_(detail::make_rain_args(to_string(wkl_cat)))
+	  args_(detail::make_rain_args(to_string(wkl_cat))),
+	  ready_(false)
 	{
 	}
 
 	public: rain_workload_driver(workload_category wkl_cat,
 								 ::std::string const& rain_home)
 	: cmd_(detail::make_java_command()),
-	  args_(detail::make_rain_args(to_string(wkl_cat), rain_home))
+	  args_(detail::make_rain_args(to_string(wkl_cat), rain_home)),
+	  ready_(false)
 	{
 	}
 
@@ -150,7 +177,8 @@ class rain_workload_driver: public base_workload_driver
 								 ::std::string const& rain_home,
 								 ::std::string const& java_home)
 	: cmd_(detail::make_java_command(java_home)),
-	  args_(detail::make_rain_args(to_string(wkl_cat), rain_home))
+	  args_(detail::make_rain_args(to_string(wkl_cat), rain_home)),
+	  ready_(false)
 	{
 	}
 
@@ -161,7 +189,8 @@ class rain_workload_driver: public base_workload_driver
 								 FwdIterT arg_first,
 								 FwdIterT arg_last)
 	: cmd_(detail::make_java_command(java_home)),
-	  args_(detail::make_rain_args(to_string(wkl_cat), rain_home, arg_first, arg_last))
+	  args_(detail::make_rain_args(to_string(wkl_cat), rain_home, arg_first, arg_last)),
+	  ready_(false)
 	{
 	}
 
@@ -177,34 +206,104 @@ class rain_workload_driver: public base_workload_driver
 							"Unknown workload category");
 	}
 
+	private: void ready(bool val)
+	{
+		::pthread_mutex_lock(p_mutex_);
+			ready_ = val;
+		::pthread_mutex_unlock(p_mutex_);
+	}
+
+	private: sys_process_pointer process()
+	{
+		return p_proc_;
+	}
+
+	private: sys_process_pointer process() const
+	{
+		return p_proc_;
+	}
+
 	private: void do_start()
 	{
 		if (p_proc_ && p_proc_->alive())
 		{
 			p_proc_->terminate();
 		}
+
+		ready_ = false;
 		p_proc_ = ::boost::make_shared<sys_process_type>(cmd_);
 
 		p_proc_->asynch(true);
-		p_proc_->run(args_.begin(), args_.end());
+		p_proc_->run(args_.begin(), args_.end(), false, false, true);
 
 		if (p_proc_->status() != ::dcs::system::running_process_status)
 		{
 		   DCS_EXCEPTION_THROW(::std::runtime_error,
 							   "Unable to start RAIN workload driver");
 		}
+
+		//TODO: create a thread that monitor the output of the process
+		::pthread_create(p_thread_, 0, &detail::monitor_rain_rampup, this);
 	}
 
 	private: void do_stop()
 	{
 		p_proc_->terminate();
+
+		if (::pthread_join(*p_thread_, 0) != 0)
+		{
+			DCS_EXCEPTION_THROW(::std::runtime_error,
+								"Unable to join thread");
+		}
+		p_thread_ = 0;
+	}
+
+	private: bool do_alive() const
+	{
+		return p_proc_->alive();
+	}
+
+	private: bool do_ready() const
+	{
+		return ready_;
 	}
 
 
 	private: ::std::string cmd_;
 	private: ::std::vector< ::std::string > args_;
 	private: sys_process_pointer p_proc_;
+	private: bool ready_;
+	private: ::pthread_t* p_thread_;
+	private: ::pthread_mutex_t* p_mutex_;
 }; // rain_workload_driver
+
+namespace detail {
+
+inline
+void* monitor_rain_rampup(void* arg)
+{
+	rain_workload_driver* p_driver = static_cast<rain_workload_driver*>(arg);
+
+	::std::istream& eis = p_driver->process()->error_stream();
+
+	while (eis.good())
+	{
+		::std::string line;
+
+		::std::getline(eis, line);
+
+		// Look for "Ramp up finished!" string
+		if (line.find("Ramp up finished") != ::std::string::npos)
+		{
+			p_driver->ready(true);
+			break;
+		}
+	}
+
+	return 0;
+}
+
+} // Namespace detail
 
 }} // Namespace dcs::testbed
 
