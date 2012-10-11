e95b80d71bcff5faaef014216af86a6e0a7a1c91
sguazt <marco.guazzone@gmail.com>
2012-10-11 19:26:14 +0200

    (change:minor) Added license information.

--- inc/dcs/testbed/sawtooth_signal_generator.hpp
+++ inc/dcs/testbed/sawtooth_signal_generator.hpp
@@ -1,3 +1,35 @@
+/**
+ * \file dcs/testbed/sawtooth_signal_generator.hpp
+ *
+ * \brief Generates sawtooth signals.
+ *
+ * \author Marco Guazzone (marco.guazzone@gmail.com)
+ *
+ * <hr/>
+ *
+ * Copyright (C) 2012       Marco Guazzone
+ *                          [Distributed Computing System (DCS) Group,
+ *                           Computer Science Institute,
+ *                           Department of Science and Technological Innovation,
+ *                           University of Piemonte Orientale,
+ *                           Alessandria (Italy)]
+ *
+ * This file is part of dcsxx-testbed.
+ *
+ * dcsxx-testbed is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * dcsxx-testbed is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with dcsxx-testbed. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef DCS_TESTBED_SAWTOOTH_SIGNAL_GENERATOR_HPP
 #define DCS_TESTBED_SAWTOOTH_SIGNAL_GENERATOR_HPP
 
--- inc/dcs/testbed/virtual_machines.hpp
+++ inc/dcs/testbed/virtual_machines.hpp
@@ -1,3 +1,35 @@
+/**
+ * \file dcs/testbed/virtual_machines.hpp
+ *
+ * \brief Includes all virtual machine classes.
+ *
+ * \author Marco Guazzone (marco.guazzone@gmail.com)
+ *
+ * <hr/>
+ *
+ * Copyright (C) 2012       Marco Guazzone
+ *                          [Distributed Computing System (DCS) Group,
+ *                           Computer Science Institute,
+ *                           Department of Science and Technological Innovation,
+ *                           University of Piemonte Orientale,
+ *                           Alessandria (Italy)]
+ *
+ * This file is part of dcsxx-testbed.
+ *
+ * dcsxx-testbed is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * dcsxx-testbed is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with dcsxx-testbed. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef DCS_TESTBED_VIRTUAL_MACHINES_HPP
 #define DCS_TESTBED_VIRTUAL_MACHINES_HPP
 
