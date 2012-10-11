f872444275f5b3a860cb4aa5290cda4cf6da1159
sguazt <marco.guazzone@gmail.com>
2012-10-11 19:27:44 +0200

    (new:major) Added new signal generators: 'square' and 'uniform' signal generators

--- inc/dcs/testbed/square_signal_generator.hpp
+++ inc/dcs/testbed/square_signal_generator.hpp
@@ -1,7 +1,7 @@
 /**
- * \file dcs/testbed/signal_generators.hpp
+ * \file dcs/testbed/square_signal_generator.hpp
  *
- * \brief Includes all signal generator classes.
+ * \brief Generates a square signal.
  *
  * \author Marco Guazzone (marco.guazzone@gmail.com)
  *
@@ -30,17 +30,55 @@
  * along with dcsxx-testbed. If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef DCS_TESTBED_SIGNAL_GENERATORS_HPP
-#define DCS_TESTBED_SIGNAL_GENERATORS_HPP
+#ifndef DCS_TESTBED_SQUARE_SIGNAL_GENERATOR_HPP
+#define DCS_TESTBED_SQUARE_SIGNAL_GENERATOR_HPP
 
 
 #include <dcs/testbed/base_signal_generator.hpp>
-#include <dcs/testbed/constant_signal_generator.hpp>
-#include <dcs/testbed/gaussian_signal_generator.hpp>
-#include <dcs/testbed/sawtooth_signal_generator.hpp>
-#include <dcs/testbed/sinusoidal_signal_generator.hpp>
-#include <dcs/testbed/square_signal_generator.hpp>
-#include <dcs/testbed/uniform_signal_generator.hpp>
 
 
-#endif // DCS_TESTBED_SIGNAL_GENERATORS_HPP
+namespace dcs { namespace testbed {
+
+template <typename ValueT>
+class square_signal_generator: public base_signal_generator<ValueT>
+{
+	private: typedef base_signal_generator<ValueT> base_type;
+	public: typedef ValueT value_type;
+	public: typedef typename base_type::vector_type vector_type;
+
+
+	public: square_signal_generator(vector_type const& ul, vector_type const& uh)
+	: ul_(uh),
+	  uh_(ul),
+	  low_(false)
+	{
+	}
+
+ 
+	private: vector_type do_generate()
+	{
+		low_ = !low_;
+
+		if (!low_)
+		{
+			return ul_;
+		}
+
+		return uh_;
+	}
+
+
+	private: void do_reset()
+	{
+		low_ = false;
+	}
+
+
+	private: vector_type ul_; ///< Low-state values
+	private: vector_type uh_; ///< High-state values
+	private: bool low_; ///< Flag to control the high/low generation phase
+}; // square_signal_generator
+
+}} // Namespace dcs::testbed
+
+#endif // DCS_TESTBED_SQUARE_SIGNAL_GENERATOR_HPP
---
+++ inc/dcs/testbed/uniform_signal_generator.hpp
@@ -0,0 +1,101 @@
+/**
+ * \file dcs/testbed/uniform_signal_generator.hpp
+ *
+ * \brief Generates signals according to a uniform probability distribution.
+ *
+ * \author Marco Guazzone (marco.guazzone@gmail.com)
+ *
+ * <hr/>
+ *
+ * Copyright (C) 2012       Marco Guazzone
+ *                          [Distributed Computing System (DCS) Group,
+ *                           Computer Science Institute,
+ *                           Department of Science and Technological Innovation,
+ *                           University of Piemonte Orientale,
+ *                           Alessandria (Italy)]
+ *
+ * This file is part of dcsxx-testbed.
+ *
+ * dcsxx-testbed is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * dcsxx-testbed is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with dcsxx-testbed. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef DCS_TESTBED_UNIFORM_SIGNAL_GENERATOR_HPP
+#define DCS_TESTBED_UNIFORM_SIGNAL_GENERATOR_HPP
+
+
+#include <boost/numeric/ublas/operation/size.hpp>
+#include <boost/random/uniform_real_distribution.hpp>
+#include <cstddef>
+#include <dcs/assert.hpp>
+#include <dcs/testbed/base_signal_generator.hpp>
+
+
+namespace dcs { namespace testbed {
+
+template <typename ValueT, typename RandomGeneratorT>
+class uniform_signal_generator: public base_signal_generator<ValueT>
+{
+	private: typedef base_signal_generator<ValueT> base_type;
+	public: typedef ValueT value_type;
+	public: typedef typename base_type::vector_type vector_type;
+	private: typedef ::boost::random::uniform_real_distribution<value_type> uniform_distribution_type;
+	private: typedef ::std::vector<uniform_distribution_type> uniform_distribution_container;
+	private: typedef RandomGeneratorT random_generator_type;
+
+
+	public: uniform_signal_generator(vector_type const& u_min, vector_type const& u_max, random_generator_type& rng)
+	: rng_(rng)
+	{
+		namespace ublas = ::boost::numeric::ublas;
+
+		// pre: size(u_min) == size(u_max)
+		DCS_ASSERT(ublas::size(u_min) == ublas::size(u_max),
+				   DCS_EXCEPTION_THROW(::std::invalid_argument,
+									   "Size of min and max vectors does not match"));
+
+		::std::size_t n(ublas::size(u_min));
+		for (::std::size_t i = 0; i < n; ++i)
+		{
+			distrs_.push_back(uniform_distribution_type(u_min(i), u_max(i)));
+		}
+	}
+
+ 
+	private: vector_type do_generate()
+	{
+		::std::size_t n(distrs_.size());
+		vector_type u(n);
+		for (::std::size_t i = 0; i < n; ++i)
+		{
+			u(i) = distrs_[i](rng_);
+		}
+
+		return u;
+	}
+
+
+	private: void do_reset()
+	{
+		// do nothing: we should reset the random number generator, but this should be done elsewhere.
+	}
+
+
+	private: random_generator_type& rng_;
+	private: uniform_distribution_container distrs_;
+}; // uniform_signal_generator
+
+}} // Namespace dcs::testbed
+
+
+#endif // DCS_TESTBED_UNIFORM_SIGNAL_GENERATOR_HPP
