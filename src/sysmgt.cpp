50a2a25dd67546f07436d099ebccea73dcdd1074
sguazt <marco.guazzone@gmail.com>
2013-01-24 19:05:35 +0100

    (change:minor) Cosmetic changes to license statement.

--- src/sysmgt.cpp
+++ src/sysmgt.cpp
@@ -14,20 +14,20 @@
  *                           University of Piemonte Orientale,
  *                           Alessandria (Italy)]
  *
- * This file is part of dcsxx-testbed.
+ * This file is part of dcsxx-testbed (below referred to as "this program").
  *
- * dcsxx-testbed is free software: you can redistribute it and/or modify
+ * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published
  * by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * dcsxx-testbed is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with dcsxx-testbed.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <boost/numeric/ublas/banded.hpp>
