f8f209bf662ff77fe5eb050903c850dcab01b500
sguazt <marco.guazzone@gmail.com>
2013-12-08 22:16:14 +0100

    (new;minor) Added options to set SLO value and control time (change:minor) Cosmetic changes

--- src/sysmgt.cpp
+++ src/sysmgt.cpp
@@ -69,33 +69,41 @@ namespace detail { namespace /*<unnamed>*/ {
 const dcs::testbed::workload_category default_workload(dcs::testbed::olio_workload);
 const dcs::testbed::workload_generator_category default_workload_driver(dcs::testbed::rain_workload_generator);
 const ::std::string default_workload_driver_rain_path("/usr/local/opt/rain-workload-toolkit");
-const ::std::string default_out_dat_file("./sysmgt-out.dat");
-const double default_sampling_time(10);
+//const ::std::string default_out_dat_file("./sysmgt-out.dat");
+const double default_sampling_time(1000);
+const double default_control_time(3*default_sampling_time);
 const double default_ewma_smooth_factor(0.9);
+const double default_slo_value(0);
 
 void usage(char const* progname)
 {
 	::std::cerr << "Usage: " << progname << " [options]" << ::std::endl
 				<< " --help" << ::std::endl
 				<< "   Show this message." << ::std::endl
-				<< " --out-dat-file <file path>" << ::std::endl
-				<< "   The path to the output data file." << ::std::endl
-				<< "   [default: '" << default_out_dat_file << "']." << ::std::endl
-				<< " --ts <time in secs>" << ::std::endl
-				<< "   Sampling time (in seconds)." << ::std::endl
+//				<< " --out-dat-file <file path>" << ::std::endl
+//				<< "   The path to the output data file." << ::std::endl
+//				<< "   [default: '" << default_out_dat_file << "']." << ::std::endl
+				<< " --slo-value <value>" << ::std::endl
+				<< "   The target value for the SLO metric." << ::std::endl
+				<< "   [default: '" << default_slo_value << "']." << ::std::endl
+				<< " --tc <time in millisecs>" << ::std::endl
+				<< "   Control time (in milliseconds)." << ::std::endl
+				<< "   [default: " << default_control_time << "]." << ::std::endl
+				<< " --ts <time in millisecs>" << ::std::endl
+				<< "   Sampling time (in milliseconds)." << ::std::endl
 				<< "   [default: " << default_sampling_time << "]." << ::std::endl
 				<< " --verbose" << ::std::endl
 				<< "   Show verbose messages." << ::std::endl
 				<< "   [default: disabled]." << ::std::endl
 				<< " --vm-uri <URI>" << ::std::endl
 				<< "   The VM URI to connect." << ::std::endl
-				<< "   Repeat this option as many times as is the number of your VMs."
+				<< "   Repeat this option as many times as is the number of your VMs." << ::std::endl
 				<< " --wkl <name>" << ::std::endl
 				<< "   The workload to generate. Possible values are: 'olio', 'rubis'." << ::std::endl
-				<< "   [default: '" << default_workload << "']." << ::std::endl
+				<< "   [default: '" << ::dcs::testbed::to_string(default_workload) << "']." << ::std::endl
 				<< " --wkl-driver <name>" << ::std::endl
 				<< "   The workload driver to use. Possible values are: 'rain'." << ::std::endl
-				<< "   [default: '" << default_workload_driver << "']." << ::std::endl
+				<< "   [default: '" << ::dcs::testbed::to_string(default_workload_driver) << "']." << ::std::endl
 				<< " --wkl-driver-rain-path <name>" << ::std::endl
 				<< "   The full path to the RAIN workload driver." << ::std::endl
 				<< "   [default: '" << default_workload_driver_rain_path << "']." << ::std::endl
@@ -132,26 +140,30 @@ int main(int argc, char *argv[])
 	typedef unsigned int uint_type;
 	typedef testbed::traits<real_type,uint_type> traits_type;
 
-	bool help(false);
-	std::string out_dat_file;
-	real_type ts;
-	bool verbose(false);
-	testbed::workload_category wkl;
-	testbed::workload_generator_category wkl_driver;
-	std::string wkl_driver_rain_path;
-	std::vector<std::string> vm_uris;
+	bool opt_help(false);
+//	std::string opt_out_dat_file;
+	real_type opt_ts;
+	real_type opt_tc;
+	bool opt_verbose(false);
+	testbed::workload_category opt_wkl;
+	testbed::workload_generator_category opt_wkl_driver;
+	std::string opt_wkl_driver_rain_path;
+	std::vector<std::string> opt_vm_uris;
+	real_type opt_slo_value(0);
 
 	// Parse command line options
 	try
 	{
-		help = dcs::cli::simple::get_option(argv, argv+argc, "--help");
-		out_dat_file = dcs::cli::simple::get_option<std::string>(argv, argv+argc, "--out-dat-file", detail::default_out_dat_file);
-		ts = dcs::cli::simple::get_option<real_type>(argv, argv+argc, "--ts", detail::default_sampling_time);
-		verbose = dcs::cli::simple::get_option(argv, argv+argc, "--verbose");
-		vm_uris = dcs::cli::simple::get_options<std::string>(argv, argv+argc, "--vm-uri");
-		wkl = dcs::cli::simple::get_option<testbed::workload_category>(argv, argv+argc, "--wkl", detail::default_workload);
-		wkl_driver = dcs::cli::simple::get_option<testbed::workload_generator_category>(argv, argv+argc, "--wkl-driver", detail::default_workload_driver);
-		wkl_driver_rain_path = dcs::cli::simple::get_option<std::string>(argv, argv+argc, "--wkl-driver-rain-path", detail::default_workload_driver_rain_path);
+		opt_help = dcs::cli::simple::get_option(argv, argv+argc, "--help");
+//		opt_out_dat_file = dcs::cli::simple::get_option<std::string>(argv, argv+argc, "--out-dat-file", detail::default_out_dat_file);
+		opt_tc = dcs::cli::simple::get_option<real_type>(argv, argv+argc, "--tc", detail::default_control_time);
+		opt_ts = dcs::cli::simple::get_option<real_type>(argv, argv+argc, "--ts", detail::default_sampling_time);
+		opt_verbose = dcs::cli::simple::get_option(argv, argv+argc, "--verbose");
+		opt_vm_uris = dcs::cli::simple::get_options<std::string>(argv, argv+argc, "--vm-uri");
+		opt_wkl = dcs::cli::simple::get_option<testbed::workload_category>(argv, argv+argc, "--wkl", detail::default_workload);
+		opt_wkl_driver = dcs::cli::simple::get_option<testbed::workload_generator_category>(argv, argv+argc, "--wkl-driver", detail::default_workload_driver);
+		opt_wkl_driver_rain_path = dcs::cli::simple::get_option<std::string>(argv, argv+argc, "--wkl-driver-rain-path", detail::default_workload_driver_rain_path);
+		opt_slo_value = dcs::cli::simple::get_option<real_type>(argv, argv+argc, "--slo-value", detail::default_slo_value);
 	}
 	catch (std::exception const& e)
 	{
@@ -163,7 +175,7 @@ int main(int argc, char *argv[])
 		return EXIT_FAILURE;
 	}
 
-	if (help)
+	if (opt_help)
 	{
 		detail::usage(argv[0]);
 		return EXIT_SUCCESS;
@@ -171,38 +183,42 @@ int main(int argc, char *argv[])
 
 	int ret(0);
 
-	if (verbose)
+	if (opt_verbose)
 	{
 		std::ostringstream oss;
 
-		for (std::size_t i = 0; i < vm_uris.size(); ++i)
+		for (std::size_t i = 0; i < opt_vm_uris.size(); ++i)
 		{
 			if (i > 0)
 			{
 				oss << ", ";
 			}
-			oss << "VM URI: " << vm_uris[i];
+			oss << "VM URI: " << opt_vm_uris[i];
 		}
 		dcs::log_info(DCS_LOGGING_AT, oss.str());
 		oss.str("");
 
-		oss << "Output data file: " << out_dat_file;
+//		oss << "Output data file: " << opt_out_dat_file;
+//		dcs::log_info(DCS_LOGGING_AT, oss.str());
+//		oss.str("");
+
+		oss << "Control time: " << opt_tc;
 		dcs::log_info(DCS_LOGGING_AT, oss.str());
 		oss.str("");
 
-		oss << "Sampling time: " << ts;
+		oss << "Sampling time: " << opt_ts;
 		dcs::log_info(DCS_LOGGING_AT, oss.str());
 		oss.str("");
 
-		oss << "Workload: " << wkl;
+		oss << "Workload: " << opt_wkl;
 		dcs::log_info(DCS_LOGGING_AT, oss.str());
 		oss.str("");
 
-		oss << "Workload driver: " << wkl_driver;
+		oss << "Workload driver: " << opt_wkl_driver;
 		dcs::log_info(DCS_LOGGING_AT, oss.str());
 		oss.str("");
 
-		oss << "Workload driver RAIN path: " << wkl_driver_rain_path;
+		oss << "Workload driver RAIN path: " << opt_wkl_driver_rain_path;
 		dcs::log_info(DCS_LOGGING_AT, oss.str());
 		oss.str("");
 	}
@@ -224,9 +240,9 @@ int main(int argc, char *argv[])
 
 	try
 	{
-		const std::size_t nt(vm_uris.size()); // Number of tiers
-		const real_type rt_q99(0.2870*1.e+3);
-		const real_type rt_mean(0.1034*1.e+3);
+		const std::size_t nt(opt_vm_uris.size()); // Number of tiers
+		//const real_type rt_q99(0.2870*1.e+3);
+		//const real_type rt_mean(0.1034*1.e+3);
 
 		testbed::system_experiment<traits_type> sys_exp;
 
@@ -234,8 +250,8 @@ int main(int argc, char *argv[])
 		// - Setup application (and VMs)
 		std::map<vmm_identifier_type,vmm_pointer> vmm_map;
 		std::vector<vm_pointer> vms;
-		std::vector<std::string>::const_iterator uri_end_it(vm_uris.end());
-		for (std::vector<std::string>::const_iterator it = vm_uris.begin();
+		std::vector<std::string>::const_iterator uri_end_it(opt_vm_uris.end());
+		for (std::vector<std::string>::const_iterator it = opt_vm_uris.begin();
 			 it != uri_end_it;
 			 ++it)
 		{
@@ -258,15 +274,15 @@ int main(int argc, char *argv[])
 			vms.push_back(p_vm);
 		}
 		app_pointer p_app = boost::make_shared< testbed::application<traits_type> >(vms.begin(), vms.end());
-		p_app->slo(testbed::response_time_application_performance, detail::rt_slo_checker<real_type>(rt_q99));
+		p_app->slo(testbed::response_time_application_performance, detail::rt_slo_checker<real_type>(opt_slo_value));
 
 		// - Setup workload driver
 		app_driver_pointer p_drv;
-		switch (wkl_driver)
+		switch (opt_wkl_driver)
 		{
 			case testbed::rain_workload_generator:
 				{
-					boost::shared_ptr< testbed::rain::workload_driver<traits_type> > p_drv_impl = boost::make_shared< testbed::rain::workload_driver<traits_type> >(wkl, wkl_driver_rain_path);
+					boost::shared_ptr< testbed::rain::workload_driver<traits_type> > p_drv_impl = boost::make_shared< testbed::rain::workload_driver<traits_type> >(opt_wkl, opt_wkl_driver_rain_path);
 					p_app->register_sensor(testbed::response_time_application_performance, p_drv_impl->sensor(testbed::response_time_application_performance));
 					//p_drv = boost::make_shared< testbed::rain::workload_driver<traits_type> >(drv_impl);
 					p_drv = p_drv_impl;
@@ -297,8 +313,8 @@ int main(int argc, char *argv[])
 #endif // DCS_TESTBED_EXP_LQ_APP_MGR_USE_ALT_SS
 			testbed::lqry_application_manager<traits_type> lqry_mgr(Q, R);
 			lqry_mgr.sysid_strategy(p_sysid_alg);
-			//lqry_mgr.target_value(testbed::response_time_application_performance, rt_mean);
-			lqry_mgr.target_value(testbed::response_time_application_performance, rt_q99*(1-0.20));
+			//lqry_mgr.target_value(testbed::response_time_application_performance, rt_q99*(1-0.20));
+			lqry_mgr.target_value(testbed::response_time_application_performance, opt_slo_value);
 
 			p_mgr = boost::make_shared< testbed::lqry_application_manager<traits_type> >(lqry_mgr);
 #elif defined(DCS_TESTBED_USE_PADALA2009_APP_MGR)
@@ -313,16 +329,16 @@ int main(int argc, char *argv[])
 			sysid_strategy_pointer p_sysid_alg = boost::make_shared< testbed::rls_ff_arx_miso_proxy<traits_type> >(na, nb, nk, ny, nu, ff);
 			testbed::padala2009_application_manager<traits_type> padala2009_mgr;
 			padala2009_mgr.sysid_strategy(p_sysid_alg);
-			//padala2009_mgr.target_value(testbed::response_time_application_performance, rt_mean);
-			padala2009_mgr.target_value(testbed::response_time_application_performance, rt_q99*(1-0.20));
+			//padala2009_mgr.target_value(testbed::response_time_application_performance, rt_q99*(1-0.20));
+			padala2009_mgr.target_value(testbed::response_time_application_performance, opt_slo_value);
 			padala2009_mgr.stability_factor(q);
 
 			p_mgr = boost::make_shared< testbed::padala2009_application_manager<traits_type> >(padala2009_mgr);
 #else
 # error Application Manager not recognized
 #endif
-			p_mgr->sampling_time(static_cast<uint_type>(ts));
-			p_mgr->control_time(3*p_mgr->sampling_time());
+			p_mgr->sampling_time(opt_ts);
+			p_mgr->control_time(opt_tc);
 		}
 		p_mgr->app(p_app);
 
