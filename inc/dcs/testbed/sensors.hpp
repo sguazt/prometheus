a7da795dcb3dd5cf59d5a0dc31d1c9119d2969b2
sguazt <marco.guazzone@gmail.com>
2014-06-05 17:18:45 +0200

    (change:minor) Massive license change GPLv3 -> Apache v.2 license

--- inc/dcs/testbed/sensors.hpp
+++ inc/dcs/testbed/sensors.hpp
@@ -1,3 +1,27 @@
+/**
+ * \file dcs/testbed/sensors.hpp
+ *
+ * \brief Include all sensors
+ *
+ * \author Marco Guazzone (marco.guazzone@gmail.com)
+ *
+ * <hr/>
+ *
+ * Copyright 2012 Marco Guazzone (marco.guazzone@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #ifndef DCS_TESTBED_SENSORS_HPP
 #define DCS_TESTBED_SENSORS_HPP
 
@@ -5,6 +29,7 @@
 #include <dcs/testbed/base_sensor.hpp>
 #include <dcs/testbed/libvirt/sensors.hpp>
 #include <dcs/testbed/rain/sensors.hpp>
+#include <dcs/testbed/ycsb/sensors.hpp>
 
 
 #endif // DCS_TESTBED_SENSORS_HPP
