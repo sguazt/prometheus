cab78fbcea6a7c5d593ee39958f049347dd1d10f
sguazt <marco.guazzone@gmail.com>
2014-12-02 10:08:32 +0100

    (new) Experiments can now be configured by means of a YAML-based configuration file

---
+++ inc/dcs/testbed/configurators.hpp
@@ -0,0 +1,30 @@
+/**
+ * \file dcs/testbed/configurators.hpp
+ *
+ * \brief Configurator for building system experiments
+ *
+ * \author Marco Guazzone (marco.guazzone@gmail.com)
+ *
+ * <hr/>
+ *
+ * Copyright 2014 Marco Guazzone (marco.guazzone@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef DCS_TESTBED_CONFIGURATORS_HPP
+#define DCS_TESTBED_CONFIGURATORS_HPP
+
+#include <dcs/testbed/conf/yaml_configurator.hpp>
+
+#endif // DCS_TESTBED_CONFIGURATORS_HPP
