fbf1d4a60c730f423ff85449af17533739b4a3d0
sguazt <marco.guazzone@gmail.com>
2016-02-01 09:28:39 +0100

    Tools: update dom_stats tool and added the tool dom_mem_stress to stress domain's memory

--- tools/dom_stats.cpp
+++ tools/dom_stats.cpp
@@ -5,6 +5,8 @@
 #include <cstdlib>
 #include <cstring>
 #include <ctime>
+#include <dcs/testbed/libvirt/detail/utility.hpp>
+#include "dom_stats.hpp"
 #include <iomanip>
 #include <iostream>
 #include <libvirt/libvirt.h>
@@ -13,6 +15,24 @@
 #include <stdexcept>
 #include <string>
 #include <unistd.h>
+
+
+namespace testbed = dcs::testbed;
+
+
+#if 0 /* DEPRECATED */
+
+#if DCS_TESTBED_SENSOR_HAVE_MEMINFO_SERVER
+# include <boost/array.hpp>
+# include <boost/asio.hpp>
+# include <boost/cstdint.hpp>
+# include <boost/system/error_code.hpp>
+# include <boost/system/system_error.hpp>
+# include <boost/tuple/tuple.hpp>
+# include <jsoncpp/json/value.h>
+# include <jsoncpp/json/reader.h>
+#endif // DCS_TESTBED_SENSOR_HAVE_MEMINFO_SERVER
+
 
 namespace utility {
 
@@ -42,6 +62,25 @@ namespace utility {
 
 	return err_str;
 }
+
+#if DCS_TESTBED_SENSOR_HAVE_MEMINFO_SERVER
+
+inline
+boost::tuple<boost::uint32_t, std::string> meminfo_unpack(char const* input)
+{
+    boost::tuple<boost::uint32_t, std::string> result;
+
+    boost::uint32_t sz = ntohl(*reinterpret_cast<boost::uint32_t const*>(input));
+    boost::get<0>(result) = sz;
+    input += 1 * sizeof(boost::uint32_t);
+
+    boost::get<1>(result) = std::string(reinterpret_cast<char const*>(input), sz);
+
+    return result;
+}
+
+#endif // DCS_TESTBED_SENSOR_HAVE_MEMINFO_SERVER
+
 
 virConnectPtr connect(::std::string const& uri)
 {
@@ -305,16 +344,23 @@ class dom_stats
 		}
 		else
 		{
+			// Update CPU utilization stats
 			::boost::uint64_t ns_elapsed = (cur_time_.tv_sec-prev_time_.tv_sec)*1.0e9+(cur_time_.tv_nsec-prev_time_.tv_nsec);
 			::boost::uint64_t ns_used = cur_node_info_.cpuTime-prev_node_info_.cpuTime;
 //	::std::cerr << "DEBUG> %CPU: " << (ns_used/ns_elapsed) << ::std::endl;
-			pct_cpu_ = static_cast<double>(ns_used/static_cast<long double>(ns_elapsed))*100.0;
+			pct_cpu_ = 100.0*static_cast<double>(ns_used/static_cast<long double>(ns_elapsed));
+
+			// Update RAM utilization stats
 //	::std::cerr << "DEBUG> %RAM: " << (cur_node_info_.memory/cur_node_info_.maxMem) << ::std::endl;
+			const unsigned long cfg_max_mem = this->config_max_memory();
+			const unsigned long cur_max_mem = this->max_memory();
+			const unsigned long cur_mem = this->current_memory();
+			bool ok = true;
 			virDomainMemoryStatStruct stats[VIR_DOMAIN_MEMORY_STAT_NR];
 			int nr_stats = virDomainMemoryStats(dom_, stats, VIR_DOMAIN_MEMORY_STAT_NR, VIR_DOMAIN_AFFECT_CURRENT);
 			if (0 <= nr_stats)
 			{
-				long double mem_avail_MB = 0;
+				long double mem_avail = 0;
 				for (int i = 0; i < nr_stats; ++i)
 				{
 					if (stats[i].tag == VIR_DOMAIN_MEMORY_STAT_SWAP_IN)
@@ -340,7 +386,7 @@ class dom_stats
 					if (stats[i].tag == VIR_DOMAIN_MEMORY_STAT_AVAILABLE)
 					{
 						std::cerr << "DEBUG> MEM available " << stats[i].val << std::endl;
-						mem_avail_MB = stats[i].val/1024.0;
+						mem_avail = stats[i].val;
 					}
 					if (stats[i].tag == VIR_DOMAIN_MEMORY_STAT_ACTUAL_BALLOON)
 					{
@@ -351,10 +397,135 @@ class dom_stats
 						std::cerr << "DEBUG> MEM rss " << stats[i].val << std::endl;
 					}
 
-					pct_ram_ = static_cast<double>(mem_avail_MB/cur_node_info_.maxMem)*100.0;
+					pct_ram_ = 100.0*static_cast<double>((cur_max_mem-mem_avail)/static_cast<long double>(cfg_max_mem));
 				}
 			}
 			else
+			{
+				std::cerr << "DEBUG> CONFIG MAX MEM: " << cfg_max_mem << std::endl;
+				std::cerr << "DEBUG> CURRENT MAX MEM: " << cur_max_mem << std::endl;
+				std::cerr << "DEBUG> CURRENT MEM: " << cur_mem << std::endl;
+
+#ifdef DCS_TESTBED_SENSOR_HAVE_MEMINFO_SERVER
+				try
+				{
+					namespace asio = boost::asio;
+
+					std::string server_addr = this->domain_name();
+					std::string server_port = DCS_TESTBED_SENSOR_MEMINFO_SERVER_PORT;
+
+					std::cerr << "Connecting to " << server_addr << " on port " << server_port << std::endl;
+
+					asio::io_service io_service;
+
+					asio::ip::tcp::resolver resolver(io_service);
+					asio::ip::tcp::resolver::query query(server_addr, server_port);
+					asio::ip::tcp::resolver::iterator endpoint_iterator = resolver.resolve(query);
+
+					asio::ip::tcp::socket socket(io_service);
+					boost::asio::connect(socket, endpoint_iterator);
+
+					std::ostringstream oss;
+					for (;;)
+					{
+						boost::array<char, 1024> buf;
+						boost::system::error_code error;
+
+						const std::size_t len = socket.read_some(boost::asio::buffer(buf), error);
+						if (error == boost::asio::error::eof)
+						{
+							break; // Connection closed cleanly by peer.
+						}
+						else if (error)
+						{
+							throw boost::system::system_error(error); // Some other error.
+						}
+						oss << std::string(buf.data(), len);
+					}
+					socket.close();
+
+					if (!oss.str().empty())
+					{
+						boost::uint32_t sz;
+						std::string meminfo;
+
+						boost::tie(sz, meminfo) = utility::meminfo_unpack(oss.str().c_str());
+
+						std::cerr << "DEBUG> MEMINFO: " << meminfo << std::endl;
+
+						Json::Value root;   // will contains the root value after parsing
+						Json::Reader reader;
+						bool parse_ok = reader.parse(meminfo, root);
+						if (parse_ok)
+						{
+							long double mem_avail = 0;
+							if (root.isMember("MemAvailable"))
+							{
+								std::istringstream iss;
+								iss.str(root.get("MemAvailable", "").asString());
+								iss >> mem_avail;
+							}
+							else if (root.isMember("MemFree"))
+							{
+								long double mem_free = 0;
+								std::istringstream iss;
+								iss.str(root.get("MemFree", "").asString());
+								iss >> mem_free;
+								long double mem_cache = 0;
+								iss.str(root.get("Cached", "0").asString());
+								iss >> mem_cache;
+
+								mem_avail = mem_free+mem_cache;
+							}
+							long double mem_tot = 0;
+							if (root.isMember("MemTotal"))
+							{
+								std::istringstream iss;
+								iss.str(root.get("MemTotal", "").asString());
+								iss >> mem_tot;
+							}
+							else
+							{
+								mem_tot = cur_mem;
+							}
+
+							if (root.isMember("Committed_AS"))
+							{
+								long double mem_committed = 0;
+								std::istringstream iss;
+								iss.str(root.get("Committed_AS", "").asString());
+								iss >> mem_committed;
+
+								if (mem_avail > (mem_tot-mem_committed))
+								{
+									std::cerr << "DEBUG> COMMITTED: " << mem_committed << " => Adjust Available Memory: "  << mem_avail << " -> " << (mem_tot-mem_committed) << std::endl;
+									mem_avail = mem_tot-mem_committed;
+								}
+							}
+
+							pct_ram_ = 100.0*static_cast<double>((mem_tot-mem_avail)/static_cast<long double>(cfg_max_mem));
+							//std::cerr << "DEBUG> Mem Tot: " << mem_tot << ", Mem Avail: " << mem_avail << ", Cfg Max Mem: " << cfg_max_mem << " -> MEM%: " << pct_ram_ << std::endl;
+						}
+						else
+						{
+							throw std::runtime_error("Unexpected format for JSON file");
+						}
+					}
+					else
+					{
+						ok = false;
+					}
+				}
+				catch (std::exception const& e)
+				{
+					ok = false;
+				}
+#else // DCS_TESTBED_SENSOR_HAVE_MEMINFO_SERVER
+				ok = false
+#endif // DCS_TESTBED_SENSOR_HAVE_MEMINFO_SERVER
+			}
+
+			if (!ok)
 			{
 				//::std::ostringstream oss;
 				//oss << "Failed to get domain memory stats: " << utility::last_error(conn_);
@@ -362,11 +533,8 @@ class dom_stats
 
 				std::clog << "[warning] Failed to get precise domain memory stats: " << utility::last_error(conn_) << std::endl;
 
-				std::cerr << "DEBUG> MEM current (from node info): " << cur_node_info_.memory << std::endl;
-				std::cerr << "DEBUG> MEM max (from node info): " << cur_node_info_.maxMem << std::endl;
-				std::cerr << "DEBUG> MEM max (from API): " << virDomainGetMaxMemory(dom_) << std::endl;
-
-				pct_ram_ = static_cast<double>(cur_node_info_.memory/static_cast<long double>(cur_node_info_.maxMem))*100.0;
+				//pct_ram_ = 100.0*static_cast<double>(cur_node_info_.memory/static_cast<long double>(cur_node_info_.maxMem));
+				pct_ram_ = 100.0*static_cast<double>(cur_mem/static_cast<long double>(cfg_max_mem));
 			}
 		}
 	}
@@ -384,6 +552,62 @@ class dom_stats
 	public: std::string hostname() const
 	{
 		return hname_;
+	}
+
+	public: std::string domain_name() const
+	{
+		char const* ret = 0;
+
+		ret = virDomainGetName(dom_);
+		if (0 == ret)
+		{
+			std::ostringstream oss;
+			oss << "Failed to query the name for domain: " << utility::last_error(conn_);
+			throw std::runtime_error(oss.str());
+		}
+
+		return ret;
+	}
+
+	public: unsigned long config_max_memory() const
+	{
+		unsigned long mem = ::virDomainGetMaxMemory(dom_);
+		if (0 == mem)
+		{
+			std::ostringstream oss;
+			oss << "Failed to query the config max memory for domain \"" << ::virDomainGetName(dom_) << "\": " << utility::last_error(conn_);
+			throw std::runtime_error(oss.str());
+		}
+
+		return mem;
+	}
+
+	public: unsigned long max_memory() const
+	{
+		::virDomainInfo node_info;
+		int ret = ::virDomainGetInfo(dom_, &node_info);
+		if (0 > ret)
+		{
+			std::ostringstream oss;
+			oss << "Failed to query the max memory for domain \"" << ::virDomainGetName(dom_) << "\": " << utility::last_error(conn_);
+			throw std::runtime_error(oss.str());
+		}
+
+		return node_info.maxMem;
+	}
+
+	public: unsigned long current_memory() const
+	{
+		::virDomainInfo node_info;
+		int ret = ::virDomainGetInfo(dom_, &node_info);
+		if (0 > ret)
+		{
+			std::ostringstream oss;
+			oss << "Failed to query the current memory for domain \"" << ::virDomainGetName(dom_) << "\": " << utility::last_error(conn_);
+			throw std::runtime_error(oss.str());
+		}
+
+		return node_info.memory;
 	}
 
 	private: int max_num_cpus()
@@ -427,6 +651,9 @@ class dom_stats
 	private: std::string hname_; ///< The domain host name
 }; // dom_stats
 
+#endif // DEPRECATED
+
+
 int main(int argc, char* argv[])
 {
 	std::string uri = "xen:///";
@@ -443,22 +670,23 @@ int main(int argc, char* argv[])
 
 	try
 	{
-		virConnectPtr conn = utility::connect(uri);
+		virConnectPtr conn = testbed::libvirt::detail::connect(uri);
 
-		virDomainPtr dom = utility::connect_domain(conn, dom_name);
+		virDomainPtr dom = testbed::libvirt::detail::connect_domain(conn, dom_name);
 
 		dom_stats stats(conn, dom);
 
-		std::cout << "DOMAIN: " << dom_name << " (hostname: " << stats.hostname() << ")" << std::endl;
+		std::cout << "DOMAIN: " << dom_name << " (hostname: " << testbed::libvirt::detail::domain_hostname(conn, dom) << ")" << std::endl;
 
-		std::cout << "#vCPUs: " << stats.num_vcpus(VIR_DOMAIN_AFFECT_CURRENT) << std::endl;
-		std::cout << "#CPUs: " << stats.num_cpus(VIR_DOMAIN_AFFECT_CURRENT) << std::endl;
+		std::cout << "#vCPUs: " << testbed::libvirt::detail::num_vcpus(conn, dom, VIR_DOMAIN_AFFECT_CURRENT) << std::endl;
+		std::cout << "#CPUs: " << testbed::libvirt::detail::num_cpus(conn, dom, VIR_DOMAIN_AFFECT_CURRENT) << std::endl;
 
 		for (std::size_t i = 0; /*i < n*/true; ++i)
 		{
-			::sleep(5);
+			::sleep(1);
 			stats.collect();
 			std::cout << "#" << i << " STATS:" << std::endl;
+			std::cout << "  Time: " << std::time(0) << std::endl;
 			std::cout << "  %vCPU: " << std::fixed << stats.percent_cpu() << "%" << std::endl;
 			std::cout << "  %RAM: " << std::fixed << stats.percent_ram() << "%" << std::endl;
 		}
